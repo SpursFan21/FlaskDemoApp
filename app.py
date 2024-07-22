f577c239bfc96c6c28a1cc9c12b4427dca5c0d6f
SpursFan21 <duncanfrederic@gmail.com>
2024-07-23 11:13:42 +1200

    migrations and db connect + venv setup

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from flask import Flask
  
 app = Flask(__name__)
  
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:Cellfone01@localhost:5432/flaskdemodb"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/postgres'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
  
 db.init_app(app)
